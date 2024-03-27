12e7459ce11632d63675f77404c067c0a195b833
akhilvarma1 <61343260+akhilvarma1@users.noreply.github.com>
2024-03-27 09:36:49 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 
 ReactDOM.render(
   <>
-  <h1>Akhil Varma</h1>
+  
   <nav className="navbar navbar-expand-sm bg-light  navbar-dark fixed-top">  
   <ul className="navbar-nav">
     <li className="nav-item">
       <a className="nav-link text-primary" href="#section1" style={{fontFamily: 'Pacifico, cursive'}}>About</a>
     </li>
     <li className="nav-item">
-      <a className="nav-link text-primary" href="#section2" style={{fontFamily: 'Pacifico, cursive'}}>Education</a>
+      <a className="nav-link text-primary" href="#section2" style={{fontFamily: 'Pacifico, cursive'}}>Professional Experience</a>
+    </li>
+    <li className="nav-item">
+      <a className="nav-link text-primary" href="#section2a" style={{fontFamily: 'Pacifico, cursive'}}>Education</a>
     </li>
     <li className="nav-item ">
       <a className="nav-link text-primary" style={{fontFamily: 'Pacifico, cursive'}} href="#section3">Skills</a>
@@ -33,32 +36,47 @@ ReactDOM.render(
 <div className="card bg-light text-dark">
     <div className="card-body">
     <h1 style={{fontFamily: 'Dela Gothic One ,cursive'}}><div className="text-primary" style={{FontSize:'50px'}} >Akhil</div>Varma </h1>
-    <h6>Spl-D 1010,P v colony,Manuguru,507125 <span className="text-primary">Yedarasriakhilvarm123@gmail.com</span>
+    <h6>Hyderabad, Telangana. <span className="text-primary">Yedarasriakhilvarm123@gmail.com</span>
     </h6>
     <p style={{fontFamily: 'Roboto Condensed, sans-serif',fontSize:'25px'}}>I am a simple computer geek, like to go in accordance with nature. I am passionate about Software Development and most of my time is spent in that, I love to solve daily life problems with software technologies. I keep myself updated with opensource tools, development and feel pleasure to work in that field.</p>
     <a href="https://www.youtube.com/" className="fa fa-facebook"></a>
 <a href="https://www.youtube.com/" className="fa fa-twitter"></a>
-<a href="https://www.youtube.com/" className="fa fa-linkedin"></a>
+<a href="https://www.linkedin.com/in/akhil-varma-6a5a121a6/" className="fa fa-linkedin"></a>
 <a href="https://www.youtube.com/" className="fa fa-instagram"></a>
-<a href="https://www.youtube.com/" className="fa fa-github"></a>  
+<a href="https://github.com/akhilvarma1" className="fa fa-github"></a>  
     </div>
   </div>
 </div>
 
 </div>
 
+<div id="section2a" className="container-fluid bg-light" style={{paddingTop:'70px',paddingBottom:'70px'}}>
+  <h1 className="text-primary" style={{fontFamily: 'Dela Gothic One, cursive',paddingBottom:'40px'}}>Work Experience</h1>
+  <div className="card bg-primary text-white container crd2">
+    <div className="card-body" >
+    <h4>Tata Consultancy Services</h4>
+    <p style={{fontFamily: 'Roboto Condensed, sans-serif',fontSize:'25px'}}>Systems Engineer<br/><div style={{fontSize:'18px'}}>January 2023- Currently Working.</div><br/><div style={{fontSize:'19px'}}>Working as Oracle Database adminstrator,Proficient in implementing and managing high availability (HA) and 
+    disaster recovery (DR) solutions such as Oracle Data Guard and Oracle RAC, ensuring business continuity and data resilience.</div> </p>
+    <hr style={{backgroundColor:'white'}} />
+    <h4>Keka HR</h4>
+    <p style={{fontFamily: 'Roboto Condensed,sans-serif', fontSize : '25px'}}>Quality Analyst<br/> <div style={{fontSize:'18px'}}>January 2022-October 2022</div> <br/><div style={{fontSize:'19px'}}> Tested Multiple HR based application,Recognized for the ability to visualize complex testing scenarios and dependencies through mind maps, facilitating 
+    comprehensive test coverage and thorough defect analysis.</div></p>    
+    </div>
+  </div>
+ </div>
+
 <div id="section2" className="container-fluid bg-light" style={{paddingTop:'70px',paddingBottom:'70px'}}>
   <h1 className="text-primary" style={{fontFamily: 'Dela Gothic One, cursive',paddingBottom:'40px'}}>Education</h1>
   <div className="card bg-primary text-white container crd2">
     <div className="card-body" >
     <h4>MLRITM</h4>
-    <p style={{fontFamily: 'Roboto Condensed, sans-serif',fontSize:'25px'}}>Bachelor of Technology<br/><br/> Information Technology<br/>CGPA :pursuing</p>
+    <p style={{fontFamily: 'Roboto Condensed, sans-serif',fontSize:'25px'}}>Bachelor of Technology<br/> <div style={{fontSize:'19px'}}>Information Technology<br/>2018-2022</div></p>
     <hr style={{backgroundColor:'white'}} />
     <h4>Sri Chaitanya Junior College</h4>
-    <p style={{fontFamily: 'Roboto Condensed,sans-serif', fontSize : '25px'}}>Intermediate<br/><br/> MPC <br/>Percentage:90.9%</p>
+    <p style={{fontFamily: 'Roboto Condensed,sans-serif', fontSize : '25px'}}>Intermediate<br/><div style={{fontSize:'19px'}}>  MPC <br/>Percentage:90.9%</div></p>
     <hr style={{backgroundColor:'white'}} />
     <h4>Dr kkr Gowtham Concept School</h4>
-    <p style={{fontFamily: 'Roboto Condensed, sans-serif',fontSize:'25px'}}>High School<br/><br/>CBSE(Central Board Of Secondary Education)<br/>Percentage:96%</p>
+    <p style={{fontFamily: 'Roboto Condensed, sans-serif',fontSize:'25px'}}>High School<br/><div style={{fontSize:'19px'}}> CBSE(Central Board Of Secondary Education)<br/>Percentage:96%</div></p>
     
     </div>
   </div>
@@ -70,22 +88,22 @@ ReactDOM.render(
     <h4>Programing Language and Skills</h4>
     <hr className="bg-primary"/>
     <div className="container">
-  <h5>
-  <span className="badge badge-primary">C</span>
-  <span className="badge badge-primary">C++</span>
-  <span className="badge badge-primary">Java</span>
-  <span className="badge badge-primary">Python</span>
-  <span className="badge badge-primary">JavaScript</span>
-  <span className="badge badge-primary">SQL</span>
-  <span className="badge badge-primary">Html</span>
-  <span className="badge badge-primary">CSS</span><br/>
-  <span className="badge badge-primary">Bootstrap</span>
-  <span className="badge badge-primary">React</span>
-  <span className="badge badge-primary">Node js</span>
-  <span className="badge badge-primary">Php</span>
-  <span className="badge badge-primary">Flutter</span>
-  <span className="badge badge-primary">Android Studio and Visual Studio</span><br/>
-  <span className="badge badge-primary">Data Structures</span>
+  <h5 >
+  <span style={{marginLeft:'2px'}} className="badge badge-primary">C</span>
+  <span style={{marginLeft:'2px'}} className="badge badge-primary">C++</span>
+  <span style={{marginLeft:'2px'}} className="badge badge-primary">Java</span>
+  <span style={{marginLeft:'2px'}} className="badge badge-primary">Python</span>
+  <span style={{marginLeft:'2px'}} className="badge badge-primary">JavaScript</span>
+  <span style={{marginLeft:'2px'}} className="badge badge-primary">SQL</span>
+  <span style={{marginLeft:'2px'}} className="badge badge-primary">Html</span>
+  <span style={{marginLeft:'2px'}} className="badge badge-primary">CSS</span><br/>
+  <span style={{marginLeft:'2px'}} className="badge badge-primary">Bootstrap</span>
+  <span style={{marginLeft:'2px'}} className="badge badge-primary">React</span>
+  <span style={{marginLeft:'2px'}} className="badge badge-primary">Node js</span>
+  <span style={{marginLeft:'2px'}} className="badge badge-primary">Php</span>
+  <span style={{marginLeft:'2px'}} className="badge badge-primary">Flutter</span>
+  <span style={{marginLeft:'2px'}} className="badge badge-primary">Android Studio and Visual Studio</span><br/>
+  <span style={{marginLeft:'2px'}} className="badge badge-primary">Data Structures</span>
   </h5>
 </div>
 
@@ -112,7 +130,8 @@ ReactDOM.render(
   <div className="card bg-light text-primary container">
     <div className="card-body">
     <h4>Acheivements<hr style={{backgroundColor:'white'}}/></h4>
-
+    <p style={{fontFamily: 'Roboto Condensed, sans-serif',fontSize:'25px'}}>Certification from Oracle Corporation as Oracle Certified Associate (OCA) - Database Administrator I.</p>
+    <p style={{fontFamily: 'Roboto Condensed, sans-serif',fontSize:'25px'}}>Certification from Keka HR company for Successful completion of Quality Analyst Internship.</p>
     <p style={{fontFamily: 'Roboto Condensed, sans-serif',fontSize:'25px'}}>Awarded certificate for Problem solving from Hacker Rank.</p>
     <p style={{fontFamily: 'Roboto Condensed, sans-serif',fontSize:'25px'}}>Awarded certificate for flutter  App development </p>
     <p style={{fontFamily: 'Roboto Condensed, sans-serif',fontSize:'25px'}}>Awarded certificate for Digital Marketing from goolge</p>
@@ -124,13 +143,12 @@ ReactDOM.render(
 </div>
 <div id="section6" className="container-fluid bg-light" style={{paddingTop:'70px',paddingBottom:'70px'}}>
   <div className="cont">
-  <img src="https://scontent.fmaa11-1.fna.fbcdn.net/v/t1.6435-9/165807246_2940708529587206_8940012106587058460_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=WHsPGiU3swIAX-qUvgQ&_nc_ht=scontent.fmaa11-1.fna&oh=e8a8880d590e7f401edffb0bb85e86b5&oe=6090E96B" alt="Akhil Varma" style={{width:'100%'}} />
-  <h1 className="title">Akhil Varma</h1>
+   <h1 className="title">Akhil Varma</h1>
   <p className="title">Student and developer</p>
   <p>Mlritm</p>
   <div className="ax" style={{margin: '24px 0'}}>
-    <a href="#"><i className="fa fa-twitter"></i></a>  
-    <a href="#"><i className="fa fa-linkedin"></i></a>  
+    <a href="https://github.com/akhilvarma1"><i className="fa fa-github"></i></a>  
+    <a href="https://www.linkedin.com/in/akhil-varma-6a5a121a6/"><i className="fa fa-linkedin"></i></a>  
     <a href="#"><i className="fa fa-facebook"></i></a> 
   </div>
   <p><button className="bx">Contact</button></p>
